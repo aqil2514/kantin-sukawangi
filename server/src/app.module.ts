d6dabaf265109e2c39e2889800263b51b8838241
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2025-01-26 08:57:35 +0700

    server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CartModule } from './_Modules/cart/cart.module';
 import { CheckoutModule } from './_Modules/checkout/checkout.module';
-import { MidtransModule } from './_modules/midtrans/midtrans.module';
 import { ConfigModule } from '@nestjs/config';
+import { MidtransModule } from './_Modules/midtrans/midtrans.module';
 
 @Module({
   imports: [
