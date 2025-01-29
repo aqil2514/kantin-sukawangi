619930a4d34f2e73ead4fedf0d795c1093df00df
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2025-01-30 03:30:01 +0700

    bug

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -1,61 +1,56 @@
-import NextAuth, { CredentialsSignin } from "next-auth";
+import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
 import { SupabaseAdapter } from "@auth/supabase-adapter";
-import { supabaseAdmin } from "./lib/server/supabase";
-import { signInSchema } from "./lib/zod";
-import { ZodError } from "zod";
-import { redirect } from "next/navigation";
 
 // TODO : Nanti lanjutin ini. Credential harus nyambung ke adapter
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google,
-    Credentials({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      //@ts-expect-error " ga tau ribet"
-      authorize: async (credentials):Promise<Auth.Users | undefined | null> => {
-        let userData: Auth.Users;
+    // Credentials({
+    //   credentials: {
+    //     email: {},
+    //     password: {},
+    //   },
+    //   //@ts-expect-error " ga tau ribet"
+    //   authorize: async (credentials):Promise<Auth.Users | undefined | null> => {
+    //     let userData: Auth.Users;
 
-        try {
-          console.log(credentials)
-          const validation = await signInSchema.parseAsync(
-            credentials
-          );
-          console.log(validation)
+    //     try {
+    //       console.log(credentials)
+    //       const validation = await signInSchema.parseAsync(
+    //         credentials
+    //       );
+    //       console.log(validation)
 
 
-          const { data, error } = await supabaseAdmin
-            .schema("next_auth")
-            .from("users")
-            .select()
-            .eq("email", validation.email)
-            .single();
+    //       const { data, error } = await supabaseAdmin
+    //         .schema("next_auth")
+    //         .from("users")
+    //         .select()
+    //         .eq("email", validation.email)
+    //         .single();
 
-            console.log(data)
+    //         console.log(data)
 
-          if (!data) {
-            throw new CredentialsSignin("User tidak ditemukan");
-          }
-          if (error) {
-            throw new Error("Terjadi kesalahan:", error);
-          }
+    //       if (!data) {
+    //         throw new CredentialsSignin("User tidak ditemukan");
+    //       }
+    //       if (error) {
+    //         throw new Error("Terjadi kesalahan:", error);
+    //       }
 
-          userData = data;
-          if(!userData.password) return redirect("/eror")
+    //       userData = data;
+    //       if(!userData.password) return redirect("/eror")
 
-          return userData;
-        } catch (error) {
-          if (error instanceof ZodError) {
-            console.log(error)
-            return null;
-          }
-        }
-      },
-    }),
+    //       return userData;
+    //     } catch (error) {
+    //       if (error instanceof ZodError) {
+    //         console.log(error)
+    //         return null;
+    //       }
+    //     }
+    //   },
+    // }),
   ],
   adapter: SupabaseAdapter({
     url: process.env.SUPABASE_URL ?? "",
