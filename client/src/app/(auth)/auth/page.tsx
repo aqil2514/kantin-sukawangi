f784ce11580a5152b28a1bfcd624cad7433213c4
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2025-02-02 17:40:29 +0700

    redirect if session is there

--- client/src/app/(auth)/auth/page.tsx
+++ client/src/app/(auth)/auth/page.tsx
@@ -1,10 +1,14 @@
+import { auth } from "@/auth";
 import Authentication from "@/components/_pages/Auth";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Autentikasi",
 };
 
-export default async function auth() {
+export default async function authPage() {
+  const session = await auth();
+  if(session) redirect("/")
   return <Authentication />;
 }
